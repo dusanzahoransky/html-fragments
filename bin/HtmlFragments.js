75968e07063318b6f2aa4602369073393ccb84b3
dusan <dusan@rezdy.com>
2021-09-16 22:20:51 +1000

    fix exec commnad

--- bin/HtmlFragments.js
+++ bin/HtmlFragments.js
@@ -1,16 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.HtmlFragments = void 0;
 const fs_1 = require("fs");
 const path_1 = require("path");
 class HtmlFragments {
     processDir(inDir, outDir, recursive = false) {
-        if (!(0, fs_1.existsSync)(outDir)) {
-            (0, fs_1.mkdirSync)(outDir);
+        if (!fs_1.existsSync(outDir)) {
+            fs_1.mkdirSync(outDir);
         }
-        const files = (0, fs_1.readdirSync)(inDir, { withFileTypes: true });
+        const files = fs_1.readdirSync(inDir, { withFileTypes: true });
         for (const file of files) {
-            const filePath = (0, path_1.resolve)(inDir, file.name);
+            const filePath = path_1.resolve(inDir, file.name);
             if (file.isDirectory()) {
                 if (recursive) {
                     this.processDir(filePath, outDir, recursive);
@@ -19,21 +18,21 @@ class HtmlFragments {
             else {
                 console.debug(`processing file: ${filePath}`);
                 const processedContent = this.processFile(filePath);
-                const outFilePath = (0, path_1.resolve)(outDir, file.name);
-                (0, fs_1.writeFileSync)(outFilePath, processedContent);
+                const outFilePath = path_1.resolve(outDir, file.name);
+                fs_1.writeFileSync(outFilePath, processedContent);
             }
         }
     }
     processFile(file) {
-        const currentDir = (0, path_1.dirname)(file);
-        const content = ((0, fs_1.readFileSync)(file)).toString();
+        const currentDir = path_1.dirname(file);
+        const content = (fs_1.readFileSync(file)).toString();
         if (!content) {
             return '';
         }
         return content.replace(/<include \s*src\s*=\s*"(.+)"\s*\/>/g, (match, path) => this.replaceInclude(currentDir, path, match));
     }
     replaceInclude(currentDir, path, match) {
-        const replacementPath = (0, path_1.resolve)(currentDir, path);
+        const replacementPath = path_1.resolve(currentDir, path);
         try {
             return this.processFile(replacementPath);
         }
